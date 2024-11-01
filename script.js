282d64bf2dd3aad36af397b6d23dfa7ae875620c
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-11-01 08:52:36 +0800

    Display library data

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const mainContent = document.querySelector("#main-content");
+
 const myLibrary = [
   {
     title: "The Hobbit",
@@ -26,3 +28,35 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() {
   
 }
+
+function displayBooks() {
+  myLibrary.map((book) => {
+    const bookItem = document.createElement("div");
+    const header = document.createElement("div");
+    const footer = document.createElement("div");
+    const title = document.createElement("h3");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const isRead = document.createElement("p");
+    
+    bookItem.classList.add("book");
+    header.classList.add("book-header");
+    footer.classList.add("book-footer");
+
+    title.innerText = book.title;
+    author.innerText = `by ${book.author}`;
+    pages.innerText = `${book.pages} pages`;
+    isRead.innerText = `${book.read ? "Read" : "Not Read Yet"}`;
+
+    header.append(title);
+    header.append(author);
+    footer.append(pages);
+    footer.append(isRead);
+
+    bookItem.append(header);
+    bookItem.append(footer);
+    mainContent.append(bookItem);
+  });
+}
+
+displayBooks();
